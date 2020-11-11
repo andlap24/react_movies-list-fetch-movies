e893890dea14e72c08f4e51aea3cb0965bc8da3c
andlap24 <andrew.laptev24@gmail.com>
2020-11-11 14:37:00 +0200

    Updates

---
+++ precache-manifest.8af9fea1d5045696c4d754d32a95985e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6a8f77038e85c970d3f5e0f2d55e92f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3a1625601903e2e4441f",
+    "url": "./static/css/2.92730f33.chunk.css"
+  },
+  {
+    "revision": "7630f8677ac44f135d72",
+    "url": "./static/css/main.9aaf0c5f.chunk.css"
+  },
+  {
+    "revision": "3a1625601903e2e4441f",
+    "url": "./static/js/2.ca571890.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ca571890.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7630f8677ac44f135d72",
+    "url": "./static/js/main.eb2c3a02.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
